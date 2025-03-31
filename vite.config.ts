58606a176dcac634217af2be2769faca83381241
prince patel <princeefive@outlook.com>
2025-03-31 12:32:47 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/princepatel/",  // Change this to match your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
